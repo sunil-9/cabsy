14cd74eb269a22c262208b9c5d11a2f211b8207a
sunil sapkota <me@sapkotasunil.com.np>
2021-04-12 13:30:00 +0545

    edit in help fragment

--- app/src/main/java/firebase/gopool/Account/HelpFragment.java
+++ app/src/main/java/firebase/gopool/Account/HelpFragment.java
@@ -28,6 +28,9 @@ public class HelpFragment extends AppCompatActivity {
 
     private static final String TAG = "HelpFragment";
 
+
+    
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
