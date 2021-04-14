0061a2c9acf0f29354998ac42152c49dd664c349
sunil sapkota <me@sapkotasunil.com.np>
2021-04-14 12:56:42 +0545

    push form sunil

--- app/src/main/java/firebase/cabsy/Login/LoginActivity.java
+++ app/src/main/java/firebase/cabsy/Login/LoginActivity.java
@@ -165,10 +165,9 @@ public class LoginActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(mContext, "Authentication failed.",
+                            Toast.makeText(mContext, "Authentication failed. 11",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
-
 //                            mProgressBar.setVisibility(GONE);
 //                            mPleaseWait.setVisibility(GONE);
 //                            mLoadingCardView.setVisibility(GONE);
